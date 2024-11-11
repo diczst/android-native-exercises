2bf935bbdee42026f94ef9612485e53cdbfcdac4
dickyzuerst <dzuerst@gmail.com>
2024-11-11 18:57:56 +0700

    some change

--- AndroidServices/app/src/main/java/com/neonusa/androidservices/MainActivity.kt
+++ AndroidServices/app/src/main/java/com/neonusa/androidservices/MainActivity.kt
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/*
+    Ada 3 jenis services di android :
+    BBF
+    1. Background : Prosesnya tidak terlihat oleh user (
+    2. Foreground : Prosesnya terlihat oleh user (contoh : memutar audio, mendownload file)
+    3. Bound
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
